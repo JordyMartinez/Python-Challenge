f3b320b1c90c9088f348f303a2016b8dcabcb089
Vanessa Gutierrez <vanessagutierrez@Vanessas-MBP.attlocal.net>
2017-07-11 22:26:40 -0700

    first commit

---
+++ index.py
@@ -0,0 +1,20 @@
+class Taco():
+    def __init__(self, meat, topping):
+        self.meat = meat
+        self.topping = topping
+
+#menu
+meats = ["asada", "pastor", "lengua"]
+toppings = ["unions", "chile", "cheese"]
+
+meat = input('What type of meat would you prefer on your taco:')
+print('Okay one with', meat)
+
+topping = input('What type of topping would you prefer on your taco:')
+print('Okay one with', topping)
+
+
+
+
+
+
